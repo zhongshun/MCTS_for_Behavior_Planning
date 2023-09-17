5a7603f9967072c8c886e063c9f324169441fac2
zhongshun <zszhang@outlook.com>
2023-09-17 22:14:12 +0800

    remove duplicated files

--- getActorCurrPosition.m
+++
@@ -1,7 +0,0 @@
-function currentActPosition = getActorCurrPosition(trajectory, sampleTime)
-% This function is to get the current position the certain actorVehicle.
-[position, orientation,~,~,~] = lookupPose(trajectory, sampleTime);
-eulerAnglesDegrees = eulerd(orientation,'ZYX','frame');
-currentActPosition = [position(1:2) eulerAnglesDegrees(1)];
-end
-
